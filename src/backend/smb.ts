02d308d5f7831cb35c186f882592937e9ed936ee
Davvos11 <vosdavid2@gmail.com>
2021-02-08 23:15:42 +0100

    fixed various bugs and added campusnet resolving

--- src/backend/smb.ts
+++ src/backend/smb.ts
@@ -4,11 +4,11 @@ import * as path from "path";
 
 const MOUNT_PATH = '/home/david/mnt/smb'
 
-export async function smbMount(url: string, type: string) {
+export async function smbMount(url: string, type: string, triedResolving = false) {
     url = url.replace(/^smb:/, '')
 
     // If it seems to be a file instead of a dir, use the parent
-    if (url.match(/\.\S{2,3}$/).length > 0) {
+    if (url.match(/\.\S{2,3}$/)) {
         url = url.replace(/\/[^\/]+$/, '')
     }
 
@@ -34,14 +34,21 @@ export async function smbMount(url: string, type: string) {
             reject(err.message)
         }))
         child.on('exit', code => {
-            if (code === 0)
+            if (code === 0) {
                 // Return successfully
                 resolve(code)
-            else
+            } else {
                 // Remove created mount point
                 removeMountPoint(location)
-                // Throw error
-                reject(stderr)
+                // Check the error type
+                if (stderr.includes(" could not resolve address for ") && !triedResolving) {
+                    // Try again with a resolved URL (recursion go brr)
+                    resolve(smbMount(resolveCampusNet(url), type, true))
+                } else {
+                    // Throw error
+                    reject(stderr)
+                }
+            }
         })
     }))
 }
@@ -80,6 +87,11 @@ async function removeEmptyDirectories(directory: string) {
 
     // Go up a directory (if needed)
     const parent = path.dirname(directory)
-    if (!path.relative(parent, MOUNT_PATH).includes('..'))
+    if (!path.relative(MOUNT_PATH, parent).includes('..'))
         await removeEmptyDirectories(path.dirname(directory))
+}
+
+
+function resolveCampusNet(url: string) {
+    return url.replace(/\/\/([^\/]+)/, "//$1.student.utwente.nl")
 }
